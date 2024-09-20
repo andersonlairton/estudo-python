44396670b99ce0b3a48bcdc2e749ddead57420ef
Anderson Lairton <alairton@dw3tecnologia.com.br>
2024-09-20 10:50:13 -0300

    estudo tipo dados

---
+++ src/estudo/script.py
@@ -0,0 +1,16 @@
+# Inteiros
+num_inteiro = 10
+print(num_inteiro)
+
+# Flutuantes
+num_flutuante = 3.14
+print(num_flutuante)
+
+# Strings
+nome = "Python"
+print(nome)
+
+# Booleanos
+is_active = True
+print(is_active)
+
