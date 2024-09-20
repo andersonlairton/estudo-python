5cddd5ac5d82fae3cd99e0e21555acf3485716d0
Anderson Lairton <alairton@dw3tecnologia.com.br>
2024-09-20 10:50:03 -0300

    estudo operações basicas com python

---
+++ src/estudo/operacoesBasicas.py
@@ -0,0 +1,19 @@
+# Adição
+soma = 10 + 5
+print(soma)
+
+# Subtração
+subtracao = 10 - 5
+print(subtracao)
+
+# Multiplicação
+multiplicacao = 10 * 5
+print(multiplicacao)
+
+# Divisão
+divisao = 10 / 2
+print(divisao)
+
+# Potência
+potencia = 2 ** 3
+print(potencia)
